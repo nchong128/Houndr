627f57ec4021db22b48afd265e0680a8aec85021
nchong128 <nchong128@gmail.com>
2019-10-24 01:53:32 +1100

    Add file upload component

--- frontend/src/pages/Resumes/Resumes.js
+++ frontend/src/pages/Resumes/Resumes.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import UploadResume from "./UploadResume";
 
 export default function Resumes(props) {
 	return(
+		<>
+		<UploadResume/>
 		<p>Resumes</p>
+		</>
 	)
 };
---
+++ frontend/src/pages/Resumes/UploadResume.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ResumeNameInput = styled.input`
+`;
+
+const ResumeContainer = styled.div`
+	max-width: 60%;
+`;
+
+const ResumeFileUpload = () => {
+	return (
+		<div className="file">
+			<label className="file-label">
+				<input type="file" className="file-input" name="resume" />
+				<span className="file-cta">
+					<span className="file-label">
+						Choose your resume...
+					</span>
+				</span>
+			</label>
+		</div>
+	);
+};
+
+export default function UploadResume(props) {
+	return(
+		<ResumeContainer className="box">
+			<form className="form" action="">
+				<div className="columns is-1 is-variable">
+					<div className="column is-6">
+						<ResumeNameInput type="text" className="input is-rounded" placeholder="Resume name"/>
+					</div>
+					<div className="column is-4">
+						<ResumeFileUpload/>
+					</div>
+					<div className="column is-2 level-right">
+						<button className="button is-rounded is-primary">Submit</button>
+					</div>
+				</div>
+			</form>
+		</ResumeContainer>
+	)
+};
